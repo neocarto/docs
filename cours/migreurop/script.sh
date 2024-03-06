300c824b6ce1adacf722c642b02c883da26c357a
neocarto <nicolas.lambert@cnrs.fr>
2024-03-06 12:07:40 +0100

    prez migreurop

--- cours/migreurop/script.sh
+++ cours/migreurop/script.sh
@@ -3,14 +3,15 @@
 rm *.pdf
 mkdir tmp
 
-convert -geometry 1600x1600 -density 200x200 -quality 100 intro/* tmp/intro.pdf 
+convert -geometry 1600x1600 -density 200x200 -quality 100 migreurop/* tmp/migreurop.pdf 
 convert -geometry 1600x1600 -density 200x200 -quality 100 actions/* tmp/actions.pdf 
 convert -geometry 1600x1600 -density 200x200 -quality 100 camps/* tmp/camps.pdf 
+convert -geometry 1600x1600 -density 200x200 -quality 100 externalisation/* tmp/externalisation.pdf 
 convert -geometry 1600x1600 -density 200x200 -quality 100 morts/* tmp/morts.pdf 
 convert -geometry 1600x1600 -density 200x200 -quality 100 syriens/* tmp/syriens.pdf 
 convert -geometry 1600x1600 -density 200x200 -quality 100 conclusion/* tmp/conclusion.pdf 
 
-qpdf --empty --pages tmp/intro.pdf tmp/actions.pdf tmp/camps.pdf tmp/morts.pdf tmp/syriens.pdf tmp/conclusion.pdf -- migreurop.pdf
+qpdf --empty --pages tmp/migreurop.pdf tmp/actions.pdf tmp/camps.pdf tmp/externalisation.pdf tmp/morts.pdf tmp/syriens.pdf tmp/conclusion.pdf -- migreurop.pdf
 
 rm tmp/*
 rmdir tmp
